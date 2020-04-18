64bd1844730b890e09ee27ec5bd3194447c685ca
feiche <20223117@qq.com>
2020-04-18 15:30:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,5 +8,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash.git
+git clone https://github.com/pymumu/smartdns.git
 ./scripts/feeds update -a
 ./scripts/feeds install -a
